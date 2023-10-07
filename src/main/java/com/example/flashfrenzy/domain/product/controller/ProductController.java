fa59153ee0000dedeed80f7b6a137d8645d70c4e
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-10-07 20:45:55 +0900

    feat: 제품 조회 기능 및 페이지 추가

--- src/main/java/com/example/flashfrenzy/domain/product/controller/ProductController.java
+++ src/main/java/com/example/flashfrenzy/domain/product/controller/ProductController.java
@@ -1,5 +1,51 @@
 package com.example.flashfrenzy.domain.product.controller;
 
+import com.example.flashfrenzy.domain.product.dto.ProductResponseDto;
+import com.example.flashfrenzy.domain.product.entity.Product;
+import com.example.flashfrenzy.domain.product.service.ProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
 
+    private final ProductService productService;
+
+    /**
+     * 상품 리스트 조회
+     */
+    @GetMapping
+    public String getProducts(Model model) {
+        List<ProductResponseDto> productList = productService.getProducts();
+        model.addAttribute("productList", productList);
+        return "product-list";
+    }
+
+    /**
+     * 상품 검색
+     */
+    @GetMapping("/search")
+    public String searchProducts(Model model, @RequestParam(value = "query") String query) {
+        List<ProductResponseDto> productList = productService.searchProducts(query);
+        model.addAttribute("productList", productList);
+        return "product-list";
+    }
+
+    /**
+     * 상품 상세 조회
+     */
+    @GetMapping("/{id}")
+    public String detailsProduct(Model model, @PathVariable(value = "id") Long productId){
+        ProductResponseDto product = productService.detailsProduct(productId);
+        model.addAttribute("product", product);
+        return "product";
+    }
 }
--- src/main/java/com/example/flashfrenzy/domain/product/service/ProductService.java
+++ src/main/java/com/example/flashfrenzy/domain/product/service/ProductService.java
@@ -1,5 +1,34 @@
 package com.example.flashfrenzy.domain.product.service;
 
+import com.example.flashfrenzy.domain.product.dto.ProductResponseDto;
+import com.example.flashfrenzy.domain.product.entity.Product;
+import com.example.flashfrenzy.domain.product.repository.ProductRepository;
+import java.util.List;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
 
+    private final ProductRepository productRepository;
+
+    public List<ProductResponseDto> getProducts() {
+       return productRepository.findAll().stream().map(ProductResponseDto::new).toList();
+    }
+
+    public List<ProductResponseDto> searchProducts(String query) {
+        return productRepository.findAllByTitleContains(query).stream().map(ProductResponseDto::new).toList();
+    }
+
+
+    public ProductResponseDto detailsProduct(Long productId) {
+
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new IllegalArgumentException("해당 상품이 없습니다.")
+        );
+
+        return new ProductResponseDto(product);
+    }
 }
