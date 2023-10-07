1402c9904ffdbbb8f0c98768aee006ec8fdeed1a
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-10-07 17:49:14 +0900

    Feat: SwaggerConfig 수정

--- src/main/java/com/example/flashfrenzy/global/config/SwaggerConfig.java
+++ src/main/java/com/example/flashfrenzy/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.ArrayList;
