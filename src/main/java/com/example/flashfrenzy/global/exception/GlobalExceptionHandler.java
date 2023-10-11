04908e6fd0a618b3ee587b197a3479d49aa51ee7
Inhee <whdlsgml1599@gmail.com>
2023-10-11 22:09:33 +0900

    chore: .gitignore, GlobalExceptionHandler 수정

--- src/main/java/com/example/flashfrenzy/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/flashfrenzy/global/exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public String ExHandler(Exception ex) {
-        log.error(ex.getMessage());
         log.error(ex.toString());
         ex.printStackTrace();
         return "redirect:/";
