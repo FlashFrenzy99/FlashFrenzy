cf711d04dccd2fb3529627141c76e4b5fd400d7b
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-10-14 14:58:45 +0900

    test: 주문 컨트롤러 관련 test 코드 추가

---
+++ src/test/java/com/example/flashfrenzy/domain/order/controller/OrderControllerTest.java
@@ -0,0 +1,81 @@
+package com.example.flashfrenzy.domain.order.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.example.flashfrenzy.domain.basket.controller.BasketController;
+import com.example.flashfrenzy.domain.basket.controller.MockSpringSecurityFilter;
+import com.example.flashfrenzy.domain.basket.entity.Basket;
+import com.example.flashfrenzy.domain.basketProdcut.entity.BasketProduct;
+import com.example.flashfrenzy.domain.order.service.OrderService;
+import com.example.flashfrenzy.domain.user.entity.User;
+import com.example.flashfrenzy.domain.user.entity.UserRoleEnum;
+import com.example.flashfrenzy.domain.user.service.UserService;
+import com.example.flashfrenzy.global.config.WebSecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebMvcTest(controllers = OrderController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private UserService userService;
+
+    @BeforeEach
+    void init() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter()))
+                .build();
+    }
+
+    @Test
+    @DisplayName("장바구니 물품들로 주문을 생성한다.")
+    void test() throws Exception {
+        //given
+        String username = "test";
+        String password = "1234";
+        UserRoleEnum role = UserRoleEnum.USER;
+        String email = "test@hello.com";
+        User user = new User(username, password, role, email, new Basket());
+//        Basket basket = user.getBasket();
+//        basket.getList().add(new BasketProduct());
+
+        mvc.perform(
+                        post("/api/orders/{id}", 1L)
+
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/auth/users/my-page"));
+
+        //when
+
+        //then
+
+    }
+
+
+
+}
