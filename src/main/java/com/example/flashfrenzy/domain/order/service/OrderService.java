8d0cc72a7effb80bdf099da042f17311314859d9
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-10-24 19:24:11 +0900

    feat: OrderService 반환 값 수정

--- src/main/java/com/example/flashfrenzy/domain/order/service/OrderService.java
+++ src/main/java/com/example/flashfrenzy/domain/order/service/OrderService.java
@@ -7,14 +7,18 @@ import com.example.flashfrenzy.domain.basketProdcut.entity.BasketProduct;
 import com.example.flashfrenzy.domain.basketProdcut.repository.BasketProductRepository;
 import com.example.flashfrenzy.domain.event.entity.Event;
 import com.example.flashfrenzy.domain.event.repository.EventRepository;
+import com.example.flashfrenzy.domain.order.dto.OrderKafkaMessageDto;
 import com.example.flashfrenzy.domain.order.entity.Order;
 import com.example.flashfrenzy.domain.order.repository.OrderRepository;
 import com.example.flashfrenzy.domain.orderProduct.entity.OrderProduct;
+import com.example.flashfrenzy.domain.product.dto.ProductResponseDto;
 import com.example.flashfrenzy.domain.product.entity.Product;
 import com.example.flashfrenzy.domain.product.repository.ProductRepository;
 import com.example.flashfrenzy.domain.product.service.ProductService;
 import com.example.flashfrenzy.domain.user.entity.User;
 import com.example.flashfrenzy.global.redis.RedisRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -26,6 +30,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.data.domain.Page;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,9 +48,12 @@ public class OrderService {
     private final RedissonClient redissonClient;
     private final ProductRepository productRepository;
     private final ProductService productService;
-    
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+
     @Transactional
-    public Long orderBasketProducts(Long id) {
+    public void orderBasketProducts(Long id) {
         log.debug("장바구니 상품 주문");
         Basket basket = basketRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 장바구니가 존재하지 않습니다.")
@@ -62,13 +71,26 @@ public class OrderService {
 
         List<OrderProduct> orderProductList = basketProductList.stream().map(basketProduct -> {
             if (eventIdList.contains(basketProduct.getProduct().getId())) {
-                Event event = eventRepository.findById(basketProduct.getProduct().getId())
-                        .orElseThrow();
+                Event event = eventRepository.findByProductId(basketProduct.getProduct().getId())
+                        .orElseThrow(
+                                () -> new IllegalArgumentException("해당 이벤트 상품이 없습니다.")
+                        );
                 return new OrderProduct(basketProduct, event.getSaleRate());
             } else {
                 return new OrderProduct(basketProduct);
             }
         }).toList();
+
+//        OrderKafkaMessageDto orderKafkaMessageDto = new OrderKafkaMessageDto(orderProductList,
+//                user.getId());
+//        String orderRequest = orderKafkaMessageDto.toJSON();
+//        System.out.println(orderRequest);
+//        System.out.println("============================kafka 메세지 전송 시작.");
+//        long startTime = System.currentTimeMillis();
+//
+//        kafkaTemplate.send("order", orderRequest);
+//        log.info("카프카 메세지 전송 elapsed time : " + (System.currentTimeMillis() - startTime) + "ms.");
+//        System.out.println("============================kafka 메세지 전송 완료.");
 
         Order order = new Order();
         order.addUser(user); // 여기 내부 수정했었음
@@ -83,17 +105,19 @@ public class OrderService {
             }
             product.discountStock(orderProduct.getCount());
 
-//            productService.discountStock(product.getId(), orderProduct.getCount());
         }
+
+
+
+
+
+
 
         Order savedOrder = orderRepository.save(order);
 
         //주문 후 장바구니 내역 삭제
 //        basketProductRepository.deleteAllByBasket(basket);
-
-        return savedOrder.getId();
-
-
+        
     }
 
 }
