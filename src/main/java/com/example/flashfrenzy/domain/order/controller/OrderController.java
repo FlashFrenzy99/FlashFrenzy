af444b3bdbd1574a5d5044789894e8c48f51d9c0
Inhee <whdlsgml1599@gmail.com>
2023-11-01 19:51:59 +0900

    fix : 리다이렉트 원복

--- src/main/java/com/example/flashfrenzy/domain/order/controller/OrderController.java
+++ src/main/java/com/example/flashfrenzy/domain/order/controller/OrderController.java
@@ -14,14 +14,11 @@ public class OrderController {
 
     private final OrderService orderService;
     @PostMapping("/{id}")
-    @ResponseBody
     public String orderBasketProducts(@PathVariable(value = "id") Long basketId) {
         long startTime = System.currentTimeMillis();
         orderService.orderBasketProducts(basketId);
         log.debug("장바구니 주문 elapsed time : "  + (System.currentTimeMillis() - startTime) + "ms.");
-        return "hello";
-//        return "redirect:/api/baskets";
-
+        return "redirect:/api/baskets";
     }
 
 
