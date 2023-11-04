0601fbc490b6cdacb83ddec26fc005a9a1c95206
LeeJiHun1 <dlwlgnsaaa@naver.com>
2023-11-04 11:52:31 +0900

    fix : 테스트를 위해서 OrderController return hello로 변경

--- src/main/java/com/example/flashfrenzy/domain/order/controller/OrderController.java
+++ src/main/java/com/example/flashfrenzy/domain/order/controller/OrderController.java
@@ -14,11 +14,13 @@ public class OrderController {
 
     private final OrderService orderService;
     @PostMapping("/{id}")
+    @ResponseBody
     public String orderBasketProducts(@PathVariable(value = "id") Long basketId) {
         long startTime = System.currentTimeMillis();
         orderService.orderBasketProducts(basketId);
         log.debug("장바구니 주문 elapsed time : "  + (System.currentTimeMillis() - startTime) + "ms.");
-        return "redirect:/api/baskets";
+        return "hello";
+        //return "redirect:/api/baskets";
     }
 
 
